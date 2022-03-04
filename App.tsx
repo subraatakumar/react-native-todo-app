46b0a4a72489cc2b91fa7116e15c2acc549030de
Subrat Kumar Das <business.ttrc@gmail.com>
2022-03-04 20:34:53 +0530

    right left arrow date added

--- App.tsx
+++ App.tsx
@@ -17,7 +17,11 @@ import {
   Text,
   useColorScheme,
   View,
+  Pressable,
+  TextInput,
 } from 'react-native';
+import {ArrowLeft2, ArrowRight2, Verify, Trash} from 'iconsax-react-native';
+import {format, addDays} from 'date-fns';
 
 import {
   Colors,
@@ -27,88 +31,43 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [navBarDate, setNavBarDate] = React.useState(new Date());
+  const [todos, setTodos] = React.useState([]);
+  
+  React.useEffect(() => {}, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleRightPress = () => {
+    setNavBarDate(addDays(navBarDate, 1));
   };
 
+  const handleLeftPress = () => {
+    setNavBarDate(addDays(navBarDate, -1));
+  };
+
+  const handleSubmitPress = () => {};
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Pressable onPress={handleLeftPress}>
+        <ArrowLeft2 color="#000000" />
+      </Pressable>
+      <Text>{format(navBarDate, 'dd/MM/yyyy')}</Text>
+      <Pressable onPress={handleRightPress}>
+        <ArrowRight2 color="#000000" />
+      </Pressable>
+      <TextInput style={styles.textinput} />
+      <Pressable onPress={handleSubmitPress}>
+        <ArrowRight2 color="#000000" />
+      </Pressable>
+      <ScrollView></ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  textinput: {
+    borderWidth: 1,
   },
 });
 
